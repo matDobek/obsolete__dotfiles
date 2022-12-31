b830d3c921640dc6a6c6bdd8cc96b14c76d47121
Mateusz Dobek <mat.dobek@gmail.com>
2022-12-31 09:18:51 +0000

    Add wrapping lines, upon entering zen mode

--- nvim/after/plugin/zenmode.lua
+++ nvim/after/plugin/zenmode.lua
@@ -1,14 +1,20 @@
 require("zen-mode").setup {
-    window = {
-        width = 90,
-        options = {
-            number = true,
-            relativenumber = true,
-        }
-    },
+  window = {
+    width = 90,
+    options = {
+      number = true,
+      relativenumber = true,
+    }
+  },
+  on_open = function(win)
+    vim.wo.wrap = true
+    vim.wo.linebreak = true
+  end,
+  on_close = function()
+    vim.wo.wrap = false
+  end,
 }
 
 vim.keymap.set("n", "<leader>zz", function()
-    require("zen-mode").toggle()
-    vim.wo.wrap = false
+  require("zen-mode").toggle()
 end)
