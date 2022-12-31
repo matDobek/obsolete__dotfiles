cfdb16212e12aa178c2ed9f39a46987199abd1fb
Mateusz Dobek <mat.dobek@gmail.com>
2022-12-31 17:58:35 +0000

    Add leading comment character upon entering new line

--- nvim/lua/matdobek/opts.lua
+++ nvim/lua/matdobek/opts.lua
@@ -7,22 +7,22 @@ vim.opt.expandtab = true      -- convert tabs to spaces
 vim.opt.smartindent = true    -- make indenting smart again
 vim.opt.autoindent = true     -- copy indentation from previous line, and apply to next one
 
-vim.opt.wrap = false          -- display long lines as just one line
-
 vim.opt.hlsearch = true       -- highlight searches
 vim.opt.incsearch = true      -- start searching when you type
 vim.opt.ignorecase = true     -- ignore case when searching
 
-vim.opt.laststatus=2          -- last window will have a status line always visible
+vim.opt.laststatus = 2        -- last window will have a status line always visible
 
 vim.opt.nu = true             -- show line numbers
 vim.opt.relativenumber = true -- display relative number
 
 vim.opt.scrolloff = 10        -- keep X lines above and below cursor when scrolling
 vim.opt.colorcolumn = "80"    -- show a vertical line at column 80
+vim.opt.textwidth = 80        -- set text width to 80 characters
+vim.opt.formatoptions = "ro"  -- :help fo-table
+-- vim.opt.wrap = false          -- display long lines as just one line
 
 vim.opt.swapfile = false      -- disable swap files
 vim.opt.backup = false        -- disable backup files
 vim.opt.undofile = true       -- keep history after exiting file
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- where to store undo files
-
